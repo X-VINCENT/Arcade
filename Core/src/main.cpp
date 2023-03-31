e36e6b022c86022c3bf043dd2e3d1dee36816fa8
X-VINCENT <xavier.vincent@epitech.eu>
2023-04-01 01:19:49 +0200

    [ADD] Check environnement (fix #18)

--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -8,9 +8,25 @@
 #include "Arcade.hpp"
 #include <string>
 
-int main(int argc, char **argv)
+int check_env(char **env)
+{
+    if (!env) {
+        std::cerr << "Error: No environment found." << std::endl;
+        return ERROR;
+    }
+    for (int i = 0; env[i] != NULL; i += 1) {
+        if (env[i] == "DISPLAY")
+            return SUCCESS;
+    }
+    std::cerr << "Error: No display found." << std::endl;
+    return ERROR;
+}
+
+int main(int argc, char **argv, char **env)
 {
     if (argc != 2)
+        return ERROR;
+    if (check_env(env) == ERROR)
         return ERROR;
     return arcade(argv[1]);
 }
