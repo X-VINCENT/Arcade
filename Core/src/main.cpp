e09bee1c0937f028f1961831a1b3946885766659
X-VINCENT <xavier.vincent@epitech.eu>
2023-04-01 21:53:54 +0200

    [ADD] Error message on bad arguments and good lib names in Cmake

--- Core/src/main.cpp
+++ Core/src/main.cpp
@@ -20,8 +20,10 @@ int check_env()
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc != 2) {
+        std::cerr << "Usage: ./arcade [lib]" << std::endl;
         return ERROR;
+    }
     if (check_env() == ERROR)
         return ERROR;
     return arcade(argv[1]);
